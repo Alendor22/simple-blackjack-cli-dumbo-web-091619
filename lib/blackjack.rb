fe32d5195e0283b30f419a9efcc6e34ecdfed726
Adeja Lendor <alendor6@gmail.com>
2019-09-18 04:36:36 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -9,8 +9,6 @@ def deal_card
   return card
 end
 
-def display_card_total
-  # code #display_card_total here
 def display_card_total(card_total)
  puts "Your cards add up to #{card_total}"
 end
@@ -25,8 +23,6 @@ def get_user_input
   gets.chomp
 end
 
-def end_game
-  # code #end_game here
 def end_game(card_total)
   puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
